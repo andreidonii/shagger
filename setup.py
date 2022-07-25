e8a1d015f8953bb3718f62ac5f6774fcd244335c
Andrei Donii <andreidonii@coherentsolutions.com>
2022-07-25 20:47:27 +0300

    first version

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='shagger',
+    version='1.0',
+    description='A kafka based draft microservice framework',
+    author='Andrei Donii',
+    author_email='569460@gmail.com',
+    packages=['shagger'],
+    install_requires=required
+)
