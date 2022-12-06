94c467a747388b2a6f1bc9266124b8422716bba7
Andrei Donii <warez@warezmac.local>
2022-12-06 13:34:36 +0100

    allow rpc

--- src/shagger.py
+++ src/shagger.py
@@ -2,6 +2,7 @@ import asyncio
 import os
 import pickle
 import traceback
+import uuid
 from json import JSONDecodeError
 from logging import getLogger
 from typing import List, Coroutine
@@ -16,7 +17,6 @@ class ShaggerBase(object):
     _should_exit = False
     _listeners = {}
     _publisher: AIOKafkaProducer = None
-    # _init = None
     _bootstrap_servers: str = None
     loop = None
 
@@ -28,8 +28,8 @@ class ShaggerBase(object):
 class App(ShaggerBase):
     __init: Coroutine = None
 
-    def __init__(self, function, *args, **kwargs):
-        topic_parts = function.__qualname__.split('.')
+    def __init__(self, function, topic=None):
+        topic_parts = (topic or function.__qualname__).split('.')
         if len(topic_parts) == 1:
             topic_parts.insert(0, DEFAULT_GROUP)
         group, topic = topic_parts
@@ -98,45 +98,58 @@ class App(ShaggerBase):
                 await consumer.stop()
 
 
-def service(function=None):
+def service(function=None, topic=None):
     if function:
-        return App(function)
+        return App(function, topic=topic)
     else:
         def wrapper(f):
-            return App(f)
+            return App(f, topic=topic)
 
         return wrapper
 
 
 class Stub(ShaggerBase):
 
-    def __init__(self, function):
-        topic_parts = function.__qualname__.split('.')
+    def __init__(self, function, topic=None, wait_result=False):
+        self.wait_result = wait_result
+        topic_parts = (topic or function.__qualname__).split('.')
         if len(topic_parts) == 1:
             topic_parts.insert(0, DEFAULT_GROUP)
         self.group, self.topic = topic_parts
         self.function = function
 
+    async def __send(self, *, publisher: AIOKafkaProducer, topic: str, value: bytes):
+        return_channel = uuid.uuid4().__str__()
+        if not self.wait_result:
+            return await publisher.send(topic=topic, value=value)
+        headers = [('return_channel', return_channel.encode())]
+        consumer = AIOKafkaConsumer([return_channel],
+                                    bootstrap_servers=ShaggerBase._bootstrap_servers
+                                    )
+        await consumer.start()
+        try:
+            await publisher.send(topic=topic, value=value, headers=headers)
+            async for msg in consumer:
+                return pickle.loads(msg.value)
+        finally:
+            await consumer.stop()
+
     async def __call__(self, *args, **kwargs):
         logger.debug('method direct call' + str([args, kwargs, self.function]))
-        # self.function(*args, **kwargs)
         if self.group == DEFAULT_GROUP:
             topic = self.topic
         else:
             topic = '.'.join([self.group, self.topic])
 
-        if ShaggerBase._publisher is None:
-            publisher = AIOKafkaProducer(
-                bootstrap_servers=ShaggerBase._bootstrap_servers or os.environ["bootstrap_servers"],
-                compression_type='gzip', acks=0)
-            await publisher.start()
-            res = await publisher.send(topic=topic, value=pickle.dumps({"args": args, "kwargs": kwargs}))
-            await publisher.stop()
-            return res
-        else:
-            # print("Sending to", '.'.join([self.group, self.topic]))
-            return await ShaggerBase._publisher.send(topic=topic,
-                                                     value=pickle.dumps({"args": args, "kwargs": kwargs}))
+        if ShaggerBase._publisher is not None:
+            return await self.__send(topic=topic,value=pickle.dumps({"args": args, "kwargs": kwargs}), publisher=ShaggerBase._publisher)
+        publisher = AIOKafkaProducer(
+            bootstrap_servers=ShaggerBase._bootstrap_servers or os.environ["bootstrap_servers"],
+            compression_type='gzip')
+        await publisher.start()
+        res = await self.__send(topic=topic, value=pickle.dumps({"args": args, "kwargs": kwargs}), publisher=publisher)
+        await publisher.stop()
+        return res
 
     @classmethod
     def run(cls, *, bootstrap_servers=None, init=None):
@@ -146,15 +159,15 @@ class Stub(ShaggerBase):
             asyncio.get_event_loop().run_until_complete(init)
 
 
-def stub(function=None):
+def stub(function=None, topic=None, wait_result=False):
     if function:
-        return Stub(function)
+        return Stub(function, topic=topic, wait_result=wait_result)
     else:
         def wrapper(f):
-            return Stub(f)
+            return Stub(f, topic=topic, wait_result=wait_result)
 
         return wrapper
 
 
-async def send(*, group=DEFAULT_GROUP, function: str, data: bytes):
-    return await ShaggerBase.send('.'.join([group, function]), data)
+async def send(*, group=DEFAULT_GROUP, function: str, data: bytes, topic=None):
+    return await ShaggerBase.send(topic or '.'.join([group, function]), data)
